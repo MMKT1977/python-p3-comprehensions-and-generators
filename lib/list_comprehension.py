e329e065ad009dbc10708d35404cf5b4b8b497a9
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-03-24 15:50:45 +0300

    even numlist

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums = [n for n in num_list if n % 2==0]
+    return even_nums
+return_evens(num_list=(1,7,9,6,13,11))
 
 def make_exclamation(sentence_list):
+    
     pass
