20519c450d752ff5916fbedf79b6cbe9153659d4
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-03-24 15:55:03 +0300

    exclaim

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -6,5 +6,5 @@ def return_evens(num_list):
 return_evens(num_list=(1,7,9,6,13,11))
 
 def make_exclamation(sentence_list):
-    
-    pass
+    return [new_sentence + "!" for new_sentence in sentence_list]
+make_exclamation(sentence_list=("Who are you","Why are you here","Tell me"))
